fe41578c0b8849397f2b0100b854cc6ca5c1a554
Thomas ICHÉ <thomasi@unity3d.com>
2018-12-10 13:54:31 +0100

    Added some other parameter binders (experimental)

---
+++ net.peeweek.vfxgraphtest/Runtime/ParameterBinders/VFXInputAxisBinder.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+namespace UnityEngine.VFX.Utils
+{
+    [AddComponentMenu("VFX/Utilities/Parameters/VFX Input Axis Binder")]
+    [VFXBinder("Input/Axis")]
+    public class VFXInputAxisBinder : VFXBinderBase
+    {
+        public string AxisParameter { get { return (string)m_AxisParameter; } set { m_AxisParameter = value; } }
+
+        [VFXParameterBinding("System.Single"), SerializeField]
+        protected ExposedParameter m_AxisParameter = "Axis";
+
+        public string AxisName = "Horizontal";
+        public float AccumulateSpeed = 1.0f;
+        public bool Accumulate = true;
+
+        public override bool IsValid(VisualEffect component)
+        {
+            return component.HasFloat(m_AxisParameter);
+        }
+
+        public override void UpdateBinding(VisualEffect component)
+        {
+            float axis = Input.GetAxisRaw(AxisName);
+
+            if (Accumulate)
+            {
+                float value = component.GetFloat(m_AxisParameter);
+                component.SetFloat(m_AxisParameter, value + (AccumulateSpeed * axis * Time.deltaTime));
+            }
+            else
+                component.SetFloat(m_AxisParameter, axis);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Axis: '{0}' -> {1}", m_AxisParameter, AxisName.ToString());
+        }
+    }
+}
