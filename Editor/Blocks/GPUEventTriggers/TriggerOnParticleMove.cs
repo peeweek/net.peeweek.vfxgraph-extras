011763013049229a079744e162f708484c69968c
Thomas Iché <4037271+peeweek@users.noreply.github.com>
2022-10-10 18:52:32 +0200

    Extra Spawner Blocks (#14)
    * Burst Multiplex FIFO / Rate Multiplex FIFO custom Spawners
    * Add Burst N Times / Correct handling of Stop Event
    * Added TriggerOnParticleMove
    * Trigger on Particle Age

---
+++ Editor/Blocks/GPUEventTriggers/TriggerOnParticleMove.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+namespace UnityEditor.VFX.Block
+{
+    [VFXInfo(category = "GPUEvent", experimental = true)]
+    class TriggerOnParticleMove : VFXBlock
+    {
+        public enum Mode
+        {
+            OverTime,
+            OverDistance
+        }
+
+        [SerializeField, VFXSetting(VFXSettingAttribute.VisibleFlags.All), Tooltip("True to allow one event max per frame")]
+        protected bool clampToOne = true;
+
+        public override string name { get { return string.Format("Trigger On Particle Move"); } }
+        public override VFXContextType compatibleContexts { get { return VFXContextType.Update; } }
+        public override VFXDataType compatibleData { get { return VFXDataType.Particle; } }
+
+        public override IEnumerable<VFXAttributeInfo> attributes
+        {
+            get
+            {
+                yield return new VFXAttributeInfo(new VFXAttribute(GetRateCountAttribute(), VFXValueType.Float), VFXAttributeMode.ReadWrite);
+                yield return new VFXAttributeInfo(VFXAttribute.EventCount, VFXAttributeMode.Write);
+
+                yield return new VFXAttributeInfo(VFXAttribute.Position, VFXAttributeMode.Read);
+                yield return new VFXAttributeInfo(VFXAttribute.OldPosition, VFXAttributeMode.Read);
+            }
+        }
+
+        public override IEnumerable<VFXNamedExpression> parameters
+        {
+            get
+            {
+                foreach (var parameter in base.parameters)
+                    yield return parameter;
+
+                yield return new VFXNamedExpression(VFXBuiltInExpression.DeltaTime, "deltaTime");
+            }
+        }
+
+        public class InputProperties
+        {
+            [Tooltip("Sets the rate of spawning particles per unit moved.")]
+            public float RatePerUnit = 10.0f;
+
+            [Tooltip("Determines the maximum distance a particle can move before being considered teleported")]
+            public float MaxFrameMoveDistance = 3f;
+        }
+
+        public class OutputProperties
+        {
+            [Tooltip("Outputs a GPU event which can connect to another system via a GPUEvent context. Attributes from the current system can be inherited in the new system.")]
+            public GPUEvent evt = new GPUEvent();
+        }
+
+        private string GetRateCountAttribute()
+        {
+            return "rateCount_" + VFXCodeGeneratorHelper.GeneratePrefix((uint)GetParent().GetIndex(this));
+        }
+
+        public override string source
+        {
+            get
+            {
+                string outSource = "";
+                string rateCount = GetRateCountAttribute();
+
+                outSource += $@"
+float delta = length(oldPosition - position);
+bool teleported = delta > MaxFrameMoveDistance;
+
+{rateCount} = teleported ? 0.0 : ({rateCount} + delta * RatePerUnit);
+uint count = floor({rateCount});
+{rateCount} = frac({rateCount});
+eventCount = count;";
+
+                if (clampToOne)
+                    outSource += @"
+eventCount = min(eventCount,1);
+";
+
+                return outSource;
+            }
+        }
+    }
+}
---
+++ Runtime/CustomSpawners/BurstMultiplexFIFO.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+namespace UnityEditor.VFX
+{
+    public class BurstMultiplexFIFO : VFXSpawnerCallbacks
+    {
+        public class InputProperties
+        {
+            [Tooltip("A multiplier applied to the spawnCount when processing the queue")]
+            public float CountScale = 1.0f;
+        }
+
+        static readonly int spawnCount = Shader.PropertyToID("spawnCount");
+        static readonly int countScale = Shader.PropertyToID("CountScale");
+
+        Queue<VFXEventAttribute> attributeQueue;
+        Queue<VFXEventAttribute> available;
+
+        public override void OnPlay(VFXSpawnerState state, VFXExpressionValues vfxValues, VisualEffect vfxComponent)
+        {
+            if (attributeQueue == null)
+                attributeQueue = new Queue<VFXEventAttribute>();
+
+            if(available == null)
+                available = new Queue<VFXEventAttribute>();
+
+            if(available.Count == 0)
+                available.Enqueue(vfxComponent.CreateVFXEventAttribute());
+
+            var attribute = available.Dequeue();
+            attribute.CopyValuesFrom(state.vfxEventAttribute);
+            attributeQueue.Enqueue(attribute);
+        }
+
+        public override void OnStop(VFXSpawnerState state, VFXExpressionValues vfxValues, VisualEffect vfxComponent)
+        {
+            while (attributeQueue.Count > 0)
+            {
+                available.Enqueue(attributeQueue.Dequeue());
+            }
+        }
+
+        public override void OnUpdate(VFXSpawnerState state, VFXExpressionValues vfxValues, VisualEffect vfxComponent)
+        {
+            if(attributeQueue.Count > 0)
+            {
+                var attribute = attributeQueue.Dequeue();
+                state.vfxEventAttribute.CopyValuesFrom(attribute);
+                available.Enqueue(attribute);
+                state.spawnCount = attribute.GetFloat(spawnCount) * vfxValues.GetFloat(countScale);
+            }
+        }
+    }
+}
